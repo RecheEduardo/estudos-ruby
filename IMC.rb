57a32e4a8eabcff93c5667a8c5c02bc4cb119cb3
RecheEduardo <Eduardorechemartins2006@gmail.com>
2024-12-28 16:20:49 -0300

    feature: adicionando condicional 'case' para definir a classificação do IMC do usuário

--- IMC.rb
+++ IMC.rb
@@ -1,9 +1,26 @@
 puts "Olá usuário! Qual é seu peso?"
-peso = gets.chomp.to_i
+peso = gets.chomp.to_f
 puts "E qual é sua altura"
 altura = gets.chomp.to_f
 
 imc = peso / (altura * altura)
 
+classificacao = "";
 
-print "Seu IMC é: #{imc.to_f.round(2)}!\n"
+case imc
+when 0...18.5
+    classificacao = "Abaixo do peso"
+when 18.5..24.9
+    classificacao = "Peso normal"
+when 25.0..29.9
+    classificacao = "Sobrepeso"
+when 30..34.9
+    classificacao = "Obesidade"
+when 35...39.9
+    classificacao = "Obesidade Grave"
+else
+    classificacao = "Obesidade Mórbida"
+end
+
+puts "Seu IMC é: #{imc.round(2)}!"
+puts "Sua classificação é: #{classificacao}"
