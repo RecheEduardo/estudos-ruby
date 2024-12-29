c320cb2f420d7fb5170c9b5ef9c14da703bcc606
RecheEduardo <Eduardorechemartins2006@gmail.com>
2024-12-29 18:09:20 -0300

    fix: arrumando identação

--- POO/banco.rb
+++ POO/banco.rb
@@ -46,14 +46,14 @@ puts ""
 puts "\t========== SISTEMA BANCÁRIO ==========\n"
 print "\n\tDigite seu nome: "
 nome = gets.chomp.to_s
-print "\n\t===============================\n"
+print "\n\t======================================\n"
 print "\n\tBem vindo, #{nome}! Agora digite o seu saldo: "
 saldo = gets.chomp.to_f
 
 cliente = ContaBancaria.new(saldo, nome)
 
 begin # As transações dentro da conta acontecem de forma sucessiva dentro da sessão begin-end-while
-    print "\n\t===============================\n"
+    print "\n\t======================================\n"
     puts "\n\tMENU DE OPÇÕES"
     puts "\n\t1 - DEPOSITAR VALOR"
     puts "\n\t2 - SACAR VALOR"
@@ -65,21 +65,21 @@ begin # As transações dentro da conta acontecem de forma sucessiva dentro da s
 
     case opcao
         when 1
-        print "\n\t===============================\n"
+        print "\n\t======================================\n"
         puts "\n\tDEPOSITAR VALOR\n\n"
         print "\n\tDigite o valor que deseja depositar: "
         valor = gets.chomp.to_f
         cliente.depositar(valor)
 
         when 2
-        print "\n\t===============================\n"
+        print "\n\t======================================\n"
         puts "\n\tSACAR VALOR\n\n"
         print "\n\tDigite o valor que deseja sacar: "
         valor = gets.chomp.to_f
         cliente.sacar(valor)
 
         when 3
-        print "\n\t===============================\n"
+        print "\n\t======================================\n"
         puts "\n\tVISUALIZAR SALDO\n\n"
         cliente.verConta
 
