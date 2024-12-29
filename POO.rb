7cf0aacff359a2edb7ac354ef5227a2ef5285605
RecheEduardo <Eduardorechemartins2006@gmail.com>
2024-12-29 13:39:33 -0300

    feature: adicionando atributo encapsulado fora do reader, e modificando por meio de métodos

--- POO.rb
+++ POO.rb
@@ -4,6 +4,11 @@
 
 class Aluno
     attr_reader :nome, :idade
+    @ultimo_nome
+
+    # attr_acessor: controle total do objeto para leitura e escrita
+    # attr_reader: apenas permite a leitura do objeto
+    # attr_writer: apenas permite a escrita do objeto
 
     # Métodos utilizados para DEFINIR os atributos do objeto
     def initialize (name, age)
@@ -18,7 +23,16 @@ class Aluno
 
     # def getIdade
     #     @idade
-    # end   
+    # end
+
+    def setUltimoNome(ultimoNome)
+        @ultimo_nome = ultimoNome
+    end
+    
+    def mostrarDados
+        "Nome: #{self.nome} #{@ultimo_nome}| Idade: #{self.idade} anos"
+    end
+    
 end
 
 # 1 -Criação de novos objetos e utilização do constructor para preencher os objetos criados
@@ -34,3 +48,13 @@ sala_de_aula = [aluno1, aluno2, aluno3, aluno4]
 sala_de_aula.each do | aluno |
     puts "#{aluno.nome}, #{aluno.idade} anos" # output: Eduardo, 18 anos
 end
+
+# 4 - Acessando diretamente pelo método do objeto
+puts("")
+puts aluno1.mostrarDados
+puts aluno2.mostrarDados
+puts aluno3.mostrarDados
+puts aluno4.mostrarDados
+
+aluno1.setUltimoNome("Reche") # Adicionando valores de acesso fora do reader
+puts aluno1.mostrarDados
